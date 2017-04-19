ac236e641a4059df02d346b0ce75afee98c3fa8f
wxw310415 <wxw310415@hotmail.com>
2017-04-20 00:14:58 +0800

    暂存 时间：2017-04-20 00:14:52

--- grails-app/jobs/TodoRepeatGeneratorJob.groovy
+++ grails-app/jobs/TodoRepeatGeneratorJob.groovy
@@ -11,48 +11,79 @@ import groovy.sql.Sql
  */
 class TodoRepeatGeneratorJob {
 
-    def dataSource
+//    def dataSource
+//
+//    static boolean isRunning = false
+//
+//    static Date currentDate = new Date ()
+//
+//    static triggers = {
+//        simple startDelay:1000*20, repeatInterval: 1000 * 60
+//    }
+//
+//    def execute () {
+//        // 运行条件：没有正在运行 + 新的一天
+//        if (isRunning) {
+//            return
+//        } else {
+//            println('repeat job execute...')
+//            if (!this.isNewDay()) {
+//                return
+//            }
+//        }
+//        println('----------------- repeat  job start --------------------')
+//        isRunning = true
+//        Sql sql  = new Sql(dataSource)
+//        TodoRepeatData todoRepeatData = new TodoRepeatData(sql)
+//        def list = todoRepeatData.fetch()
+//        todoRepeatData.generator(list)
+//        println('----------------- repeat  job end --------------------')
+//        // 初始化状态数据
+//        currentDate = new Date()
+//        isRunning = false
+//    }
+//
+//
+//    private boolean isNewDay () {
+//        // 新的日期
+//        Calendar calendar = Calendar.getInstance()
+//        int newDay = calendar.get(Calendar.DAY_OF_YEAR)
+//        // 旧的日期
+//        Calendar oldCalendar = Calendar.getInstance()
+//        oldCalendar.setTime(currentDate)
+//        int oldDay = oldCalendar.get(Calendar.DAY_OF_YEAR)
+//        if (oldDay != newDay) return true
+//        return false
+//    }
 
-    static boolean isRunning = false
-
-    static Date currentDate = new Date ()
-
+    /** 注入 */
+    def dataSource;
+    /** 当前时间为空 */
+    static Date currentDate = null;
+    /** 触发器 */
     static triggers = {
-        simple startDelay:1000*20, repeatInterval: 1000 * 60
+        // 服务器启动 1 分钟后开始运行
+        simple name: 'mySimpleTrigger', startDelay: 60000;
+        // 使用 cron 表达式进行控制：每天凌晨 00:05 进行生成
+        cron(name: "todoSendJob", cronExpression: "0 5 0 * * ?");
     }
 
-    def execute () {
-        // 运行条件：没有正在运行 + 新的一天
-        if (isRunning) {
-            return
-        } else {
-            println('repeat job execute...')
-            if (!this.isNewDay()) {
-                return
-            }
-        }
+    def execute(){
+        // 开始进行生成操作
         println('----------------- repeat  job start --------------------')
-        isRunning = true
-        Sql sql  = new Sql(dataSource)
-        TodoRepeatData todoRepeatData = new TodoRepeatData(sql)
-        def list = todoRepeatData.fetch()
-        todoRepeatData.generator(list)
+        // 设置当前运行时间
+        currentDate = new Date();
+        // 创建 sql 对象
+        Sql sql  = new Sql(dataSource);
+        // 创建重复日程生成对象
+        TodoRepeatData todoRepeatData = new TodoRepeatData(sql);
+        // 开始查询，获取到所有需要进行创建的重复日程的信息.
+        def list = todoRepeatData.fetch();
+        // 启动创建
+        todoRepeatData.generator(list);
+        // 创建结束
         println('----------------- repeat  job end --------------------')
-        // 初始化状态数据
-        currentDate = new Date()
-        isRunning = false
-    }
-
-
-    private boolean isNewDay () {
-        // 新的日期
-        Calendar calendar = Calendar.getInstance()
-        int newDay = calendar.get(Calendar.DAY_OF_YEAR)
-        // 旧的日期
-        Calendar oldCalendar = Calendar.getInstance()
-        oldCalendar.setTime(currentDate)
-        int oldDay = oldCalendar.get(Calendar.DAY_OF_YEAR)
-        if (oldDay != newDay) return true
-        return false
+        // 结束
+        return;
     }
 }
--- src/main/groovy/com/rishiqing/data/TodoRepeatData.groovy
+++ src/main/groovy/com/rishiqing/data/TodoRepeatData.groovy
@@ -57,54 +57,69 @@ class TodoRepeatData {
         this.maxOffset = maxOffset
     }
 
-
-
+    /**
+     * 查询器，用来查询需要创建重复日程的日程信息。
+     * @return
+     */
     def fetch() {
-        Date date1 =  new Date ().clearTime()
-        def list = TodoRepeatTag.createCriteria().list {
+        // 获取当前日期
+        Date startSearch =  new Date ().clearTime()
+        // 查询 repeatTag
+        def repeatTagList = TodoRepeatTag.createCriteria().repeatTagList {
             and {
                 //一直重复或者重复截止于的时间大于等于今天
                 or {
                     eq("alwaysRepeat", true)
-                    ge("repeatOverDate",date1)
+                    ge("repeatOverDate",startSearch)
                 }
+                // 没有关闭重复
                 eq("isCloseRepeat",false)
             }
         }
-        Date date2 = new Date()
-        println('search : ' + (date2.getTime() - date1. getTime()))
-        println('list size : ' + list.size())
+        Date endSearch = new Date()
+        // 搜索时长
+        println('search : ' + (endSearch.getTime() - startSearch. getTime()))
+        // 获取list的长度
+        int listSize = repeatTagList ? repeatTagList.size() : 0
+        // 重复标记的数量，每个标记对应一个重复的日程。
+        println('repeatTagList size : ' + listSize)
+        // 阀值，用来计算百分比使用
         int i = 0
-        int listSize = list ? list.size() : 0
+        // 需要进行创建的日程组成的list
         List needCreateTodos = []
-        for(def it: list){
+        // 遍历查询到的日程的list
+        for(def repeatTag: repeatTagList){
             try{
-                //获取
-                Boolean need = needCreateForToday(it,date1)
+                // 查询当前重复标记所标记的日程，是否需要进行创建操作
+                Boolean need = needCreateForToday(repeatTag,startSearch);
+                // 如果不需要进行创建
                 if(!need){
-                    continue
+                    continue;
                 }
-                long id = it.id
+                // 获取 repeatTag 的id
+                long id = repeatTag.id;
+                // 查找到 repeatTag 对应的日程
                 Todo todo  = Todo.createCriteria().get{
                     eq('repeatTagId', id)
                     sqlRestriction('1=1 order by this_.id desc limit 1')
                 }
+                // 如果日程存在，那么组成 map 添加到需要创建的日程列表里。
                 if(todo){
-                    Map map = [todo: todo,repeatTag: it,date: date1]
-
-                    needCreateTodos.add(map)
-
+                    Map map = [todo: todo,repeatTag: repeatTag,date: startSearch];
+                    needCreateTodos.add(map);
                 }
+                // 阀值 + 1
                 i ++
                 // 计算百分比
-                this.percent(listSize, i)
+                this.percent(listSize, i);
             }catch(Exception e) {
-                println "${date1.format("yyyy-MM-dd")}这天对应的重复id为${it.id}生成失败。"
+                println "${startSearch.format("yyyy-MM-dd")}这天对应的重复id为${it.id}生成失败。";
             }
         }
-        Date date3 = new Date()
-        println('fetch finish : ' + (date3.getTime() - date2.getTime()))
-        return needCreateTodos
+        // 完成所有需要创建的重复日程的查询
+        Date endFetch = new Date();
+        println('fetch finish : ' + (endFetch.getTime() - endSearch.getTime()));
+        return needCreateTodos;
     }
 
     /**
@@ -113,42 +128,62 @@ class TodoRepeatData {
      * @param secondDay    需要生成新日程的那一天
      */
     def Boolean needCreateForToday(TodoRepeatTag tag,Date secondDay){
+        // 参数错误
         if(!tag || !secondDay){
             return false
         }
+        // 如果重复标记基本时间没有，或者已经被关闭，或者没有重复类型（每天每周每月每年）
         if(!tag.repeatBaseTime || tag.isCloseRepeat || !tag.repeatType){
             return false
         }
+        // 获取重复类型（每天每周每月每年）
         String repeatType = tag.repeatType
         //每天重复
         if("everyDay".equals(repeatType)){
+
+            // 获取重复基本日期
             Date repeatBaseTime = Date.parse("yyyyMMdd",tag.repeatBaseTime)
-            return repeatBaseTime.getTime()<=secondDay.getTime()
-        }else if("everyWeek".equals(repeatType)){
-            return isSameDayInOtherWeek(tag.repeatBaseTime.split(","),secondDay)
-        }else if("everyMonth".equals(repeatType)){
-            /**
+            // 重复的基本日期是否 < 生成重复的日期
+            return repeatBaseTime.getTime()<secondDay.getTime()
+
+        }// 每周重复
+        else if("everyWeek".equals(repeatType)){
+
+            return isSameDayInOtherWeek(tag.repeatBaseTime.split(","),secondDay);
+
+        }// 如果是每月重复
+        else if("everyMonth".equals(repeatType)){
+
+            /*
              * 如果是最后一天重复则查看secondDay是否为最后一天
              * 如果不是最后一天重复则查看baseTime是否有需要创建日程的
              */
-            return tag.isLastDate?getLastDay(secondDay).getTime()==secondDay.getTime():isSameDayInOtherMonth(tag.repeatBaseTime.split(","),secondDay)
-        }else if("everyYear".equals(repeatType)){
-            return isSameDayInOtherYear(tag.repeatBaseTime,secondDay)
+            return tag.isLastDate?getLastDay(secondDay).getTime()==secondDay.getTime():isSameDayInOtherMonth(tag.repeatBaseTime.split(","),secondDay);
+        }// 如果是每年重复
+        else if("everyYear".equals(repeatType)){
+
+            return isSameDayInOtherYear(tag.repeatBaseTime,secondDay);
+
         }
-        return false
+        return false;
     }
 
     private Boolean shouldBeGenerated(Todo todo,TodoRepeatTag it,Date date1){
+        // 参数验证
         if(!todo || !it || !date1){
             return false
         }
+        // 查看当前日程是否在收纳箱，如果在，那么不为它创建
         if("inbox".equals(todo.pContainer)){
             return false
         }
+        // 查看日程是否没有 pPlanedTime ，没有则不创建
         if(!todo.pPlanedTime){
             return false
         }
+        // 日程没有被完成
         if(!todo.pIsDone){
+            // 查看是否有开始和结束时间
             if(todo.startDate && todo.endDate){
                 //如果date1的时间在上一条日程的截止时间内，则不生成
                 if(todo.startDate.getTime()<=date1.getTime()
@@ -164,26 +199,33 @@ class TodoRepeatData {
     }
     /**
      * 日程重复生成处理逻辑
-     *
-     * List< T o d o >
+     * @param list 存放日程的 list
      */
     def generator (def list) {
+        // 开始启动创建
         def resultList = []
         StringBuffer sb = new StringBuffer()
         list.each { it ->
             // 经过各种判断，如果确定需要保存到数据库，  则存入resultList中
             if (it) {
-                Todo todo = it.todo
-                TodoRepeatTag tag = it.repeatTag
-                Date date = it.date
+                // 获取日程
+                Todo todo = it.todo;
+                // 获取日程重复标记
+                TodoRepeatTag tag = it.repeatTag;
+                // 获取需要创建日程的日期
+                Date date = it.date;
+                // 判断是否应该生成重复
                 if(shouldBeGenerated(todo,tag,date)){
+                    // 需要生成的日程，添加到结果集中
                     resultList.add(todo)
+                    // 保存需要创建重复的日程的id
                     sb.append("${todo.id},")
 //                    todo.isRepeatTodo = true
 //                    todo.save(flush: true)
                 }
             }
         }
+        // 需要进行重复日程创建的日程的数量
         println('todo insert list size: ' + resultList.size())
         // 执行批量插入
         this.batchInsert(resultList)
@@ -196,8 +238,15 @@ class TodoRepeatData {
         }
     }
 
+    /**
+     * 披上插入方法
+     * @param list
+     * @return
+     */
     def batchInsert (def list = []) {
-        Date  date = new Date ()
+        // 获取当前日期
+        Date  date = new Date ();
+        //
         Connection conn = sql.getDataSource().getConnection()
         conn.setAutoCommit(false);
         String query = "INSERT INTO todo (version ,  date_created ,  last_updated ,  p_container,  p_display_order,  p_finished_time,  p_is_done,  p_note,  p_parent_id,  p_planed_time,  p_title,  p_user_id,  created_by_client,  receiver_ids,  receiver_names,  sender_id,  is_deleted,  cid,  repeat_tag_id,  sender_todo_id,  team_todo_read,  clock_alert,  kanban_item_id,  is_revoke,  closing_date_finished,  end_date,  start_date,  todo_deploy_id,  is_from_sub_todo,  is_change_date,  is_repeat_todo,  alert_every_day,  check_authority,  dates,  edit_authority,  is_archived,  inboxpcontainer, is_system)  VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
@@ -213,6 +262,11 @@ class TodoRepeatData {
         println('executeBatch:' + (date1.getTime() - date2.getTime()))
     }
 
+    /**
+     * 用来坚挺重复日程创建百分比。当为 100%　时，证明所有重复日程已经查询完毕
+     * @param size
+     * @param index
+     */
     private void percent (int size, int index) {
         int a = size
         if (index % a  == 0) {
@@ -221,7 +275,7 @@ class TodoRepeatData {
     }
 
     /**
-     * 数组中的某一天是否和targetDate的星期是相同的
+     * 数组中的某个日期对应的星期，是否与 targetDate 对应的星期是相同的
      * @param baseDates
      * @param targetDate
      * @return
@@ -230,22 +284,26 @@ class TodoRepeatData {
         if(!baseDates || !targetDate){
             return false
         }
+        // 获取当前日期的一个日历实体
         Calendar cTarget = targetDate.toCalendar()
         for(int i=0;i<baseDates.length;i++){
+            // 获取第一个基本日期 (repeatBaseTime 中的一个)
             Date baseDate = Date.parse("yyyyMMdd",baseDates[i])
             if(baseDate == null){
                 continue
             }
+            // 转换成日历格式
             Calendar cByI = baseDate.toCalendar()
+            // 如果这个基本日期的星期和 targetDate 对应的星期是相同的，返回true
             if(cTarget.get(Calendar.DAY_OF_WEEK)==cByI.get(Calendar.DAY_OF_WEEK)){
                 return true
             }
-
         }
+        // 否则
         return false
     }
     /**
-     * 数组中的某一天是否和targetDate的日是相同的
+     * 数组中的某一天是否和 targetDate 所对应的那一天是同一天（日期相同）
      * @param baseDates
      * @param targetDate
      * @return
@@ -274,18 +332,24 @@ class TodoRepeatData {
      * @return
      */
     private Boolean isSameDayInOtherYear(String baseDate,Date targetDate){
+        // 参数验证
         if(!baseDate || !targetDate){
             return false
         }
+        // 把 targetDate 转换成日历格式
         Calendar cTarget = targetDate.toCalendar()
+        // 把 repeatBaseTime 中的日期转换成日期格式
         Date b = Date.parse("yyyyMMdd",baseDate)
         if(b == null){
             return false
         }
+        // 再次转换成日历格式
         Calendar cByI = b.toCalendar()
+        // 比较是否是下一年的今天，相同则为 true
         if(cTarget.get(Calendar.DAY_OF_YEAR)==cByI.get(Calendar.DAY_OF_YEAR)){
             return true
         }
+        // 否则
         return false
     }
 
